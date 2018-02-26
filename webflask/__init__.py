c088a0f83760789cc962fef4b2e661ca6b64f84d
Michael Goldstein <michael@bitstein.org>
2018-02-26 15:55:25 -0600

    Add new gui directory for react site

--- webflask/__init__.py
+++ webflask/__init__.py
@@ -6,13 +6,13 @@ from flask import send_from_directory
 from mintools.restmin.impl.flask import create_restmin_app
 from explorer.explorer_server import API_DOMAIN
 
-GUI_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'gui')
+GUI_DIRECTORY = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', 'gui', 'public')
 
 def explorer_request_processor(app, req):
     return API_DOMAIN.resolve_request(req)
 
-app = create_restmin_app(app_name=__name__, 
-                         config_path='', 
+app = create_restmin_app(app_name=__name__,
+                         config_path='',
                          base_url='/api/v0/',
                          request_processor=explorer_request_processor)
 

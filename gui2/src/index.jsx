d75993077cabbe074b37bfbdf1a4d22bb7cd94d5
Michael Goldstein <michael@bitstein.org>
2018-03-12 12:37:29 -0500

    transaction_page.jsx: Add transaction page

--- gui2/src/index.jsx
+++ gui2/src/index.jsx
@@ -7,6 +7,7 @@ import Footer from './components/footer.jsx';
 import Navbar from './components/navbar.jsx';
 import RecentBlocks from './components/recent_blocks.jsx';
 import BlockPage from './components/block_page.jsx';
+import TransactionPage from './components/transaction_page.jsx';
 
 class Body extends Component {
     render() {
@@ -15,12 +16,11 @@ class Body extends Component {
           <div className="explorer-container">
             <div className="content-wrap">
               <Navbar />
-
-                <Switch>
-                  <Route exact path="/" component={RecentBlocks}/>
-                  <Route path="/block/:blockhash" component={BlockPage}/>
-                </Switch>
-
+              <Switch>
+                <Route exact path="/" component={RecentBlocks}/>
+                <Route path="/block/:blockhash" component={BlockPage}/>
+                <Route path="/tx/:txid" component={TransactionPage}/>
+              </Switch>
             </div>
             <Footer />
           </div>
